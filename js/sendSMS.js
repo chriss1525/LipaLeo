e577108cbb8853b51e7d275ea6e0482d654e1ae0
Erick Omondi <eriqueomondi1997@gmail.com>
2023-07-22 14:07:41 +0300

    I have set up some files for sending sms

---
+++ js/sendSMS.js
@@ -0,0 +1,14 @@
+const AfricasTalking = require('africastalking');
+
+// TODO: Initialize Africa's Talking
+const africastalking = AfricasTalking({
+  apiKey: '', 
+  username: ''
+});
+
+
+module.exports = async function sendSMS() {
+    
+    // TODO: Send message
+
+};
